05a1fea575b2d496527f0d0f02b3d38240d958cf
Owais Ibrahim <owais.ibrahim@systemsltd.com>
2019-08-03 13:43:59 -0700

    Directive

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -2,9 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'courses', //<courses> = 'courses'
-    template: '<h2>{{ title }}</h2>'
+    template: 
+        `<h2>
+            {{ title }}
+            <ul>
+                <li *ngFor="let course of courses">
+                    {{ course }}
+                </li>
+            </ul>
+        </h2>`
 }) 
 
 export class CoursesComponent {
     title = "List of courses";
+    courses = ["course1", "course2", "course3"];
 }
