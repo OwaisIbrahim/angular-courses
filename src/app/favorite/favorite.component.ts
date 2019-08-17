5c21dd0cb7f3956e98872d5a7ef8ee1dc44964c3
Owais Ibrahim <owais.ibrahim@systemsltd.com>
2019-08-17 10:54:03 -0700

    Alias to output property

--- src/app/favorite/favorite.component.ts
+++ src/app/favorite/favorite.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
+export interface FavoriteChangedEventArgs {
+  newValue: boolean
+} 
+
 @Component({
   selector: 'favorite',
   templateUrl: './favorite.component.html',
@@ -8,7 +12,7 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 export class FavoriteComponent implements OnInit {
  
   @Input('isFavorite') isSelected: boolean;
-  @Output() change = new EventEmitter();
+  @Output('change') click = new EventEmitter();
 
   constructor() { }
 
@@ -19,6 +23,6 @@ export class FavoriteComponent implements OnInit {
     this.isSelected = !this.isSelected;
     //emit to tell that something has changed 
     //this.isSelected is passed to all subscriber (where <favorite> is used)
-    this.change.emit({ newValue: this.isSelected });
+    this.click.emit({ newValue: this.isSelected });
   }
 }
