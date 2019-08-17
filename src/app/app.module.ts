ece10c3eee7736a31d8641b2dfa47f3bf38f7846
Owais Ibrahim <owais.ibrahim@systemsltd.com>
2019-08-17 08:24:12 -0700

    Input Properties
    -To mark a field as an input field do the following steps
    
    1.
    
    a.Go to favorite.component.ts and import Input in @angular/core
    
    b.Add @input() before property name eg: @input() isfavorite
    
    2.
    
    a.In favorite.component.ts  define:
    
    i.@Component({ inputs:[‘isFavorite’] })

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { SummaryPipe } from './summary.pipe';
 import { CourseComponent } from './course/course.component';
+import { FavoriteComponent } from './favorite/favorite.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CoursesComponent,
     CourseComponent,
-    SummaryPipe
+    SummaryPipe,
+    FavoriteComponent
   ],
   imports: [
     BrowserModule,
