0fc51a2697b5d1c78c2ad0bcb380f0aa0f2fcb01
Owais Ibrahim <owais.ibrahim@systemsltd.com>
2019-08-17 11:52:13 -0700

    how to apply styles - 3 ways
    STYLES PRIORITY: Template > styleUrls > styles

--- src/app/favorite/favorite.component.ts
+++ src/app/favorite/favorite.component.ts
@@ -7,20 +7,27 @@ export interface FavoriteChangedEventArgs {
 @Component({
   selector: 'favorite',
   templateUrl: './favorite.component.html',
-  styleUrls: ['./favorite.component.css']
+  styles: [
+    `
+      .glyphicon {
+        border: 1px solid;
+      padding:5px 10px 5px 40px;
+      background-repeat: no-repeat;
+      background-position: 10px center;
+      background-color: red;
+      border-radius: 3px;
+      }
+    `
+  ]
 })
-export class FavoriteComponent implements OnInit {
+export class FavoriteComponent {
  
   @Input('isFavorite') isSelected: boolean;
   @Output('change') click = new EventEmitter();
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
   onClick() {
     this.isSelected = !this.isSelected;
+    console.log("STATE CHANGED", this.isSelected)
     //emit to tell that something has changed 
     //this.isSelected is passed to all subscriber (where <favorite> is used)
     this.click.emit({ newValue: this.isSelected });
