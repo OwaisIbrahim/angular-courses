2739c693c723cdf6dd9fc96ec70d56433f82bccd
Owais Ibrahim <owais.ibrahim@systemsltd.com>
2019-08-18 01:47:48 -0700

    Directives: ngFor on courseList

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,10 @@ import { FavoriteChangedEventArgs } from './favorite/favorite.component';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  viewMode = "map";
-  changeViewMode(mode) {
-    this.viewMode = mode;
-  }
+  courses = [
+    {"id":1, "name":"course1"},
+    {"id":2, "name":"course2"},
+    {"id":3, "name":"course3"},
+    {"id":4, "name":"course4"}
+  ]
 }
