51c650b3d2f4fe84a152491a0bb17821eeeef6ea
Owais Ibrahim <owais.ibrahim@systemsltd.com>
2019-08-18 01:25:13 -0700

    Directives: ngSwitch on navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,8 @@ import { FavoriteChangedEventArgs } from './favorite/favorite.component';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  courses = [];
+  viewMode = "map";
+  changeViewMode(mode) {
+    this.viewMode = mode;
+  }
 }
