bb155350831c9d60b42c271d5a107d8ff18d0575
Owais Ibrahim <owais.ibrahim@systemsltd.com>
2019-08-19 12:22:41 -0700

    Forms: Reactive - FormBuilder - For Complex Forms

--- src/app/new-course-form/new-course-form.component.ts
+++ src/app/new-course-form/new-course-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormGroup, FormControl } from '@angular/forms';
+import { FormArray, FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'new-course-form',
@@ -7,6 +7,31 @@ import { FormArray, FormGroup, FormControl } from '@angular/forms';
   styleUrls: ['./new-course-form.component.css']
 })
 export class NewCourseFormComponent {
+  
+//FORM BUILDER
+/* 
+    form = new FormGroup({
+      name: new FormControl('', Validators.required),
+      contact: new FormGroup({
+        email: new FormControl(),
+        phone: new FormControl()
+      }),
+      topics: new FormArray([])
+    });
+        // ===
+    constructor(fb: FormBuilder) {
+      fb.group({
+        name: ['', Validators.required],
+        contact: fb.group({
+          email: [],
+          phone: []
+        }),
+        topics: ([])
+      });
+    }
+*/
+
+
   form = new FormGroup({
     topics: new FormArray([])
   });
